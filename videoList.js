4886e9bceea2b6ee160e81c1884945c80190887e
emptyelephants <emptyelephants@gmail.com>
2018-01-23 14:10:32 -0800

    Update videoList.js

--- videoList.js
+++ videoList.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const GenerateListItem = (function() {
+const videoList = (function() {
   const generateVideoItemHtml = function (video) {
     return `
 			<li>
@@ -14,15 +14,38 @@ const GenerateListItem = (function() {
     return videosString.join('');
   };
 
+    const decorateResponse = function (response) {
+    const decoratedResponse = response.items.map(youtubeObj => (
+      {
+        videoId: youtubeObj.id.videoId,
+        videoThumbnail: youtubeObj.snippet.thumbnails.default.url,
+        videoTitle: youtubeObj.snippet.title,
+      }));
+    	console.log('our deco response is',decoratedResponse);
+    	
+    return decoratedResponse;
+  };
+
   const handleSubmitForm = function() {
     $('#search-videos').on('submit', event => {
+      console.log('we got to here');
       event.preventDefault();
       const userSearch = $('#search-term').val();
       $('#search-term').val('');
-      api.fetchVideos(userSearch, api.decorateResponse);
+      api.fetchVideos(userSearch,response=>{
+      	const videos= decorateResponse(response);
+      	 store.setVideos(videos);
+      	 render();
+      }
+      );
+  
+    
+      console.log('render should have ran');
+
     });
   };
   const render = function () {
+		console.log('inside render',store.videos);
     const videoListString = generateVideoItemString(store.videos);
     $('.results').html(videoListString);
   };
